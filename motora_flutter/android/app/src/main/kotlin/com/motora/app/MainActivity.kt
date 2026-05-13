72e33339bef51dd036fdc75f6f12f653a74442c6
candylov1004 <candylov1374@gmail.com>
2026-05-13 21:53:47 +0900

    Add files via upload

---
+++ motora_flutter/android/app/src/main/kotlin/com/motora/app/MainActivity.kt
@@ -0,0 +1,7 @@
+// android/app/src/main/kotlin/com/motora/app/MainActivity.kt
+// Flutter 앱의 Android 진입점 - FlutterActivity를 그대로 상속
+package com.motora.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
